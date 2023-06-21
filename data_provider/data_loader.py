aaa4ae2fc4e98e51ccd57aaff2f511a9a480bec7
Huy0110 <duchuy01102001@gmail.com>
2023-06-21 04:23:09 +0000

    Update number of test

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -233,10 +233,9 @@ class Dataset_Custom(Dataset):
         # print(cols)
         # num_train = int(len(df_raw) * 0.7)
         # num_test = int(len(df_raw) * 0.2)
-        num_train = int(len(df_raw) * 0.9)
-        num_test = 192
+        num_train = int(len(df_raw) * 0.8)
+        num_test = int(len(df_raw) * 0.1)
         num_vali = len(df_raw) - num_train - num_test
-        train_len = int(len(df_raw) * 0.9)
         border1s = [0, num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]
         border2s = [num_train, num_train + num_vali, len(df_raw)]
         border1 = border1s[self.set_type]
