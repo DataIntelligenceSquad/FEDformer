e0e5e21c0088a44b1c4ea87a4ff3fe44310d1ff2
Huy0110 <duchuy01102001@gmail.com>
2023-06-25 09:20:29 +0000

    Update color

--- utils/tools.py
+++ utils/tools.py
@@ -88,8 +88,8 @@ def visual(true, preds=None, name='./pic/test.pdf'):
     Results visualization
     """
     plt.figure()
-    plt.plot(true, label='GroundTruth', linewidth=2)
     if preds is not None:
-        plt.plot(preds, label='Prediction', linewidth=2)
+        plt.plot(preds, label='Prediction', linewidth=2, color = 'g')
+    plt.plot(true, label='GroundTruth', linewidth=2, color='b')
     plt.legend()
     plt.savefig(name, bbox_inches='tight')
